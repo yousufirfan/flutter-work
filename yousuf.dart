c375bfbfd36339031b33f60f80415c69c9cd843a
yousufirfan <yousufkharadi64@gmail.com>
2025-10-22 16:36:11 +0500

    updated yousuf.dart file

--- yousuf.dart
+++ yousuf.dart
@@ -1,10 +1,70 @@
-void main () {
-int marks = 50;
-int attendance = 85;
-if (marks>40 && attendance>75){
-  print("Pass");
-}else {
-  print("Fail");
-}
+void main (){
+
+//int marks = 50;
+//double attendance = 85.0;
+
+//if (marks>40 && attendance>75){
+  //print("Pass");
+//}else {
+  //print("Fail");
+//}
+ 
+//int a = 45;
+//int b = 50;
+//if (a<50 && a<b){
+  //print("True");
+//}else{
+ // print("False");
+//}
+//if (a<50 || a<b){
+  //print("True");
+//}else{
+  //print("False");
+//}
+
+//String name = "Robert";
+//int urdu = 78;
+//print("urdu : $urdu");
+//int english = 45;
+//print("english : $english");
+//int mathematics = 62;
+//print("mathematics : $mathematics");
+//int total = (urdu + english + mathematics);
+//print("total : $total");
+//double percentage = (total/300*100);
+//print("percentage : $percentage");
+
+//int marks = 65;
+//if(marks>=90){
+  //print("A+");
+//}else if (marks>=80){
+  //print("A");
+//}else if (marks>=70){
+  //print("B");
+ //}else if (marks>=60){
+  //print("C");
+ //}else if (marks>=50){
+  //print("D");
+ //}else if (marks>=40){
+  //print("E");
+ //}else{
+  //print("Fail");
+ //}
+
+int biology = 65;
+int physics = 75;
+int chemistry = 53;
+int math = 94;
+       print ( "--------- MARKSHEET---------" );
+print("biology : $biology");
+print("physics : $physics");
+print("chemistry : $chemistry");
+print("math : $math");
+        print("---------total---------");
+int totalmarks = (biology + physics + chemistry + math);
+print("totalmarks : $totalmarks");
+      print("-----------------------");
+double percentage = (totalmarks/400*100);
+print("percentage : $percentage%");
 
 }
