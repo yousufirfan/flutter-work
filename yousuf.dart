312733d0b3872fc0d22a40767a202a1ab4507230
yousufirfan <yousufkharadi64@gmail.com>
2025-10-24 04:46:23 +0500

    added assingment work

--- yousuf.dart
+++ yousuf.dart
@@ -1,9 +1,109 @@
 void main (){
-  int marks = 60;
-  int attendance = 80;
-  if (marks>40 && attendance>75){
-    print("Pass");
-  } else {
-    print("Fail");
+  //int marks = 60;
+  //int attendance = 80;
+  //if (marks>40 && attendance>75){
+    //print("Pass");
+  //} else {
+    //print("Fail");
+  //}
+
+  //int a = 40;
+  //int b = 45;
+  //if (a<50 && a<b){
+    //print("True");
+  //}else{
+    //print("False");
+  //}
+  //if(a<50 || a<b){
+    //print("True");
+  //}else{
+    //print("False");
+  //}
+
+  //String Name ="Robert";
+  //print("Name:$Name");
+  //int biology = 78;
+  //print("biology:$biology");
+  //int chemistry = 45;
+  //print("chemistry:$chemistry");
+  //int physics = 62;
+  //print("physics:$physics");
+  //int total =(biology + chemistry + physics);
+  //print("total:$total");
+  //double percentage = (total/300*100);
+  //print("percentage:$percentage");
+  
+  //int marks = 40;
+  //if(marks>=90){
+    //print("A+");
+  //}else if(marks>=80){
+    //print("A");
+  //}else if(marks>=70){
+    //print("B");
+  //}else if(marks>=60){
+    //print("C");
+  //}else if(marks>=50){
+    //print("D");
+   //}else if(marks>=40){
+    //print("E");
+  //}else{
+    //print("Fail");
+  //}
+
+  //int mathematics = 75;
+  //int english = 55;
+  //int computer = 82;
+  //int chemistry = 62;
+  //print("----------MARKSHEET----------");
+  //print("mathematics:$mathematics/100");
+  //print("english:$english/100");
+  //print("computer:$computer/100");
+  //print("chemistry:$chemistry/100");
+  //print("-----------------------------");
+  //int total = (mathematics + english + computer +chemistry);
+  //print("total:$total/400");
+  //print("-----------------------------");
+  //double percentage = (total/400*100);
+  //print("percentage:$percentage%");
+  //print("-----------------------------");
+
+  //int age = 21;
+  //int age1 = 49;
+  //if(age<age1){
+    //print("21 ki umar ka banda chota hai 49 ki umar wale se");
+  //}else {
+    //print("49 ki umar ka banda bara hai 21 ki umar wale se");
+  //}
+
+  //int num = 7;
+  //if (num>0){
+    //print("number is postive");
+  //}else if (num<0){
+    //print("number is nagative");
+  //}else{
+    //print("number is zero");
+  //}
+
+  //int length = 42;
+  //int breadth = 42;
+  //if(length==breadth){
+    //print("square value");
+  //}else {
+    //print("rectangle value");
+  //}
+
+  num temperature = 42;
+  if(temperature<0){
+    print("freezing weather");
+  }else if (temperature>=0 && temperature<10){
+    print("very cold weather");
+  }else if (temperature>=10 && temperature<20){
+    print("cold weather");
+  }else if (temperature>=20 && temperature<30){
+    print("normal weather");
+  } else if (temperature>=30 && temperature<40){
+    print("its hot");
+  }else if (temperature>=40){
+    print("its very hot");
   }
 }
